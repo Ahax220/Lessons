75c04c9fa2704a8a86c16a524fbd0da1a65bb23f
Ahax220 <ahapxoren@gmail.com>
2021-04-01 23:00:27 +0500

    исправил текст в Display2()

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ namespace Lessons
 
         public static void Display2(string str)
         {
-            Console.WriteLine(str);
+            Console.WriteLine("эта строка исправлена - " + str);
 
         }
     }
