a1e35dc6e0807d831ed6ba86b313d36a757a1701
Ahax220 <ahapxoren@gmail.com>
2021-04-01 22:33:11 +0500

    Revert "Исправил ошибку в выводе"
    This reverts commit 81d46595d8526e24f712f8e2f0c4128ead13d73e.

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Lessons
 
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
+            Console.WriteLine("qwe");
             Console.WriteLine();
         }
 
