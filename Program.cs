c5691d43655f11ce920645fb39d310be0d9f370f
Ahax220 <ahapxoren@gmail.com>
2021-04-02 03:07:40 +0500

    readkey

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace Lessons
             Console.WriteLine("qwe");
             Display2("asd");
             Console.WriteLine("rty");
-            Console.ReadLine();
+            Console.ReadKey();
 
         }
 
